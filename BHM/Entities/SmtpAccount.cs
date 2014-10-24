7c3a556494fdd24b71189269b06def8096f95d62
Constantin <write.4k@gmail.com>
2014-10-23 23:05:31 -0400

    Created few entities and update dependencies package

---
+++ BHM/Entities/SmtpAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BHM.Entities
+{
+    class SmtpAccount
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public SmtpServer Server { get; set; }
+    }
+}
